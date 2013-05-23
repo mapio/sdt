3bca1ec0f6c2858ec478a8e65e734d0c1adcffe5
Massimo Santini <santini@di.unimi.it>
2013-05-23 09:50:32 +0200

    Added generation of html reports and better handling of paths

--- 3_experimenting/unitests/doit.sh
+++ 3_experimenting/unitests/doit.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
+echocol() { echo -e "\n\033[31m$@\033[0m \n" >&2; }
+
 ./clean.sh
+
+echocol "*** Compiling and running tests..."
 
 ant test
 
-cat target/test-reports/TEST-sdt.TestSuite.txt
+echocol "*** Report..."
+
+cat build/reports/TEST-sdt.TestSuite.txt
+
+cd build/reports/junit && python3 -m http.server
